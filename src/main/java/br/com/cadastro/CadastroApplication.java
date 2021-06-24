7f71a1b4a61bbfda7d73c191b96d44b67859626d
MitProAlv <72110326+MitProAlv@users.noreply.github.com>
2021-06-24 18:41:58 -0300

    mais CORS, porque não

--- src/main/java/br/com/cadastro/CadastroApplication.java
+++ src/main/java/br/com/cadastro/CadastroApplication.java
@@ -2,6 +2,10 @@ package br.com.cadastro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CadastroApplication {
@@ -10,4 +14,13 @@ public class CadastroApplication {
 		SpringApplication.run(CadastroApplication.class, args);
 	}
 
+	@Configuration
+	@EnableWebMvc
+	public class WebConfig implements WebMvcConfigurer {
+
+	    @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**");
+	    }
+	}
 }
